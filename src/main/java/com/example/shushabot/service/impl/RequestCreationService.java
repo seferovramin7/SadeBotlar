f7d90694e4379fa813c985e32b13f8ad6897dc47
Ramin Safarov <raminsafarov@Ramins-MacBook-Pro.local>
2022-05-11 15:08:13 +0400

    Leyla

--- src/main/java/com/example/shushabot/service/impl/RequestCreationService.java
+++ src/main/java/com/example/shushabot/service/impl/RequestCreationService.java
@@ -32,7 +32,7 @@ public class RequestCreationService {
         for (int i = 1; i <= 10; i++) {
             String url = urLcreator.createUrl(i);
             Boolean aBoolean = restService.generalRestService(url);
-            if (!aBoolean) {
+            if (aBoolean) {
                 messageReceiverServiceImpl.sendMessage(messageReceiverServiceImpl
                         .getNewProductMessage(1240851037L, "Şuşaya Gedirik !"));
             }
